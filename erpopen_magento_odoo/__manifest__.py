23c457b050f20ab21b54c031132fb6bb94d25d74
thom <tpeele@csolutions.nl>
2018-02-19 15:09:04 +0100

    removed line

--- erpopen_magento_odoo/__manifest__.py
+++ erpopen_magento_odoo/__manifest__.py
@@ -2,8 +2,6 @@
     'name': 'Odoo Magento Connector',
     'version': '2.3',
     'category': 'all',
-    'description': 'Magento odoo connector allows you to synchronize all the data on your magento ecommerce with odoo management. And it possible to import, export and update all your data.',
-    'summary': 'Magento odoo connector. Import customers, quotations, sales orders, products, images and stock quantity. Update all your information. Export new contacts and products directly to your Magento Ecommerce. It synchronizes in real time and automatically all the data.',
     'author': 'Thom Peele - ERP|OPEN',
     'website': 'erpopen.nl',
     'support': 'info@erpopen.nl',
